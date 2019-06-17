91d537d50f82eda3a1dbef67b5573629c3cc9c0e
Richard Graver <RchGrav@users.noreply.github.com>
2019-06-17 09:10:23 -0400

    Just some small tweaks

--- silentupdate.cmd
+++ silentupdate.cmd
@@ -3,15 +3,15 @@ SETLOCAL ENABLEEXTENSIONS
 IF ERRORLEVEL 1 ECHO Unable to enable extensions
 	
 set killThis=SVPManager.exe
-set silentSwitches=-v --silentUpdate
+set cmdSwitch=-v --silentUpdate
 set regApp={84d53064-d9b0-4084-abb8-e3f844c2279a}
 set regPath=Microsoft\Windows\CurrentVersion\Uninstall\
 
-REM  --> Check for updates
+REM Check most recent log for "found updates".
 find "found updates" %APPDATA%\SVP4\logs\active.log >nul 2>&1 
 if %ERRORLEVEL% EQU 1 goto noUpdates
 
-REM  --> Check for permissions
+REM User Administrator Detection w/ Elevation
 WHOAMI /Groups | FIND "12288" >NUL
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
@@ -20,16 +20,14 @@ if '%errorlevel%' NEQ '0' (
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
     "%temp%\getadmin.vbs"
     exit /B
-
 :gotAdmin
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
     pushd "%CD%"
     CD /D "%~dp0"
 	
-REM  --> If regHive is unset, first found from list is used
+REM Installer Registry Hive Detection
 set regHive=
 for %%R IN (HKLM\Software\ HKCU\Software\ HKLM\Software\Wow6432Node\ HKCU\Software\Wow6432Node\) DO (
 REG QUERY %%R%regPath%%regApp% >nul 2>&1 && set regHive=%%R
@@ -46,7 +44,7 @@ for /f "tokens=2 delims=," %%I in (
     'wmic process where "name='%killThis%'" get ExecutablePath^,Handle /format:csv 2^>^>nul ^| find /i "%killThis%" '
 ) do set "RELAUNCH=%%~I"
 if DEFINED RELAUNCH taskkill /F /IM %killThis% >nul 2>&1
-"%INSTALLER%" %silentSwitches%
+"%INSTALLER%" %cmdSwitch%
 goto :Done
 :noUpdates
 echo No Updates
